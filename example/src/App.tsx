818dcdaf904dc52ddba1293b9cb8c47e2dce6a3f
Jackie Lu <jackielu3@gmail.com>
2025-03-18 16:21:14 -0700

    switch to ts

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import { Img, Source } from 'uhrp-react'
+import './App.css'
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Img src="uhrp:XUT4UwibmJijUorRuhdceFhVXvpzZYfusvC6umGFHWrCxsfBK8Eq" />
+    <div className="container">
+      <h1 className="heading">UHRP React Example</h1>
 
-      <video controls>
-        <Source src="XUSy2EHiJqRxcB3frZbfQ7J1kEe1sE8x2vB6cfUr1A6rry2AeTZX" />
-      </video>
+      <div className="content">
+        <div className="media-container">
+          <h2 className="subheading">Image Preview</h2>
+          <Img
+            src="XUUVZqvzYskUvVfBZsrqXsvyoAUojQcJxDr6hTUwEz1vPLdvc64z"
+            className="image"
+            alt="Example Image"
+          />
+        </div>
+
+        <div className="media-container">
+          <h2 className="subheading">Video Preview</h2>
+          <video controls className="video">
+            <Source src="XUTEGfCykZ4E8oJhT98keoPTg2Q28Nq4sJJXLf9CYA5CjV7ZsTCV" />
+          </video>
+        </div>
+      </div>
     </div>
   )
 }
